fbdddd3f8ad7e87f13be4da4809aeb62d4516afd
Ing. Gabriel Solano <dgabrielsolo@gmail.com>
2023-11-19 16:13:13 -0600

    First commit

---
+++ a. Source code/deskpy_smart.py
@@ -0,0 +1,71 @@
+import os
+from PyPDF2 import PdfReader
+from plyer import notification
+from deskpy_util import Util
+
+class PDF():
+    def pdf_srch_text(self):
+        self.is_doc_fkyc = []       # KYC form.                     ✅ 2 pags
+        self.is_doc_ccac = []       # CICAC.                        ✅ 1 pag.
+        self.is_doc_cons = []       # Consent.                      ✅ 1 pag.
+        self.is_doc_cntr = []       # Contract.                     ✅ 2 pags.
+        self.is_doc_scrt = []       # Sign's certification.         ✅ 2 pags.
+        self.is_doc_unkn = []       # Possible unknown pages.       ✅ aux **pags.
+        self.subtree = os.listdir(self.working_folder)
+        for st in self.subtree:
+            _st = st.lower()
+            if _st.__contains__('aff'):
+                self.reading_doc = f'{self.working_folder}/{st}'
+        _pdf = open(self.reading_doc, 'rb')
+        _reader = PdfReader(_pdf)
+        _pages = _reader.pages
+        _length = _pages.__len__()
+        for n in range(_length):
+            _raw_text = _pages[n].extract_text().replace('\n',' ')
+            _raw_text = _raw_text.lower()
+            if _raw_text.__contains__('consentimiento informado') and _raw_text.__contains__('constan en este documento') and _raw_text.__contains__('me encuentro conforme') and _raw_text.__contains__('derecho a solicitar'): self.is_doc_cons.append(n)
+            elif _raw_text.__contains__('objeto del presente mandato') and _raw_text.__contains__('generar y custodiar los valores') and _raw_text.__contains__('poder especial') and _raw_text.__contains__('la firma de este documento'): self.is_doc_cntr.append(n)
+            elif _raw_text.__contains__('presente contrato') and _raw_text.__contains__('respectivo certificado') and _raw_text.__contains__('forma definitiva') and _raw_text.__contains__('intereses correspondientes'): self.is_doc_cntr.append(n)
+            elif _raw_text.__contains__('formulario') and _raw_text.__contains__('conozca a su cliente') and _raw_text.__contains__('producto o servicio') and _raw_text.__contains__('completar y firmar'): self.is_doc_fkyc.append(n)
+            elif _raw_text.__contains__('declaro') and _raw_text.__contains__('juramento') and _raw_text.__contains__('todas las consecuencias') and _raw_text.__contains__('expresamente acepto que'): self.is_doc_fkyc.append(n)
+            elif _raw_text.__contains__('consulta de datos') and _raw_text.__contains__('expediente del centro') and _raw_text.__contains__('conozca a su cliente') and _raw_text.__contains__('cicac'):
+                self.is_doc_ccac.append(n)
+                self._data_set = _pages[n].extract_text().replace('\xa0','').split('\n')
+            elif _raw_text.__contains__('consta la siguiente') and _raw_text.__contains__('generada a partir de la firma') and _raw_text.__contains__('para verificar la identidad') and _raw_text.__contains__('prueba documental'): self.is_doc_scrt.append(n)
+            elif _raw_text.__contains__('firmante') and _raw_text.__contains__('autenticado mediante') and _raw_text.__contains__('contenido a firmar disponible') and _raw_text.__contains__('url'): self.is_doc_scrt.append(n)
+            else: self.is_doc_unkn.append(n)
+        for kt in self._data_set:
+            _kt = kt.lower()
+            if _kt.__contains__('yo') and _kt.__contains__('portador de') and _kt.__contains__('de forma expresa'):
+                self._data_set = kt
+                break
+        try:
+            self._data_set = self._data_set.split(',')
+            self._data_set[0] = self._data_set[0].replace('Yo ','').replace('yo ','')
+            self.result_id = ''
+            for char in self._data_set[1]:
+                n = char.isnumeric()
+                if n: self.result_id += char
+            self.result_fn = self._data_set[0]
+            self.result_fn = self.result_fn.upper().replace('  ',' ')
+            self.result_fn = self.result_fn.split(' ')
+            sz = len(self.result_fn)
+            if sz == 2: self.result_fn = self.result_fn.reverse()
+            elif sz == 3: self.result_fn = f'{self.result_fn[-2]} {self.result_fn[-1]} {self.result_fn[0]}'
+            elif sz == 4: self.result_fn = f'{self.result_fn[-2]} {self.result_fn[-1]} {self.result_fn[0]} {self.result_fn[1]}'
+            elif sz > 4: self.result_fn = f'{self.result_fn[:-2]} {self.result_fn[-2]} {self.result_fn[-1]}'
+            self._data_set = ''
+            _pdf.close()
+        except Exception as e: notification.notify(title = f'DeskPy',message = f'Hint: {e.__class__}\n.Function: def pdf_srch_text(self)\nProcessing: self._data_set',timeout = 5)
+
+    def app_deploy(self):
+        self.id = self.editf_id.text()
+        self.fn = self.editf_fn.text()
+        Util.pdf_from_img(self)
+        Util.build_up_folders(self)
+        Util.pdf_make_merge(self)
+        Util.pdf_from_pdf(self)
+        try:
+            output_f = f'{self.sys_path.text()}{self.editf_id.text()} {self.editf_fn.text()}'
+            os.rename(self.working_folder,output_f)
+        except Exception as e: notification.notify(title = f'DeskPy',message = f'Hint: {e.__class__}\n.Function: def app_deploy(self)\nProcessing: os.rename(self.working_folder,output_f)',timeout = 5)
---
+++ a. Source code/main.py
@@ -0,0 +1,235 @@
+import os
+import sys
+from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget, QToolBar, QLabel, QVBoxLayout, QHBoxLayout, QPushButton, QLineEdit, QPlainTextEdit, QFileDialog
+from PyQt6.QtGui import QAction, QIcon
+from PyQt6.QtCore import Qt
+from plyer import notification
+from deskpy_smart import PDF
+from support import Support
+
+os.system('cls')
+
+class Main(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.init()
+        self.page()
+        self.show()
+
+    def init(self):
+        self.setWindowIcon(QIcon('icon.ico'))
+        self.setWindowTitle('DeskPy')
+        self.setMinimumWidth(900)
+        self.setMinimumHeight(500)
+
+        toolbar = QToolBar()
+        toolbar.setMovable(False)
+        self.addToolBar(toolbar)
+
+        tb_docs = QAction(QIcon('documentation.ico'), 'Documentación', self)         # tb: Go to github documentation page.
+        tb_updt = QAction(QIcon('update.ico'), 'Buscar actualizaciones', self)       # tb: Search for updates by web-scrapping.
+        tb_lstf = QAction(QIcon('folder.ico'), 'Ir al directorio de trabajo', self)  # tb: Open last folder proccesed.
+
+        tb_docs.setShortcuts(['F1','Ctrl+H'])
+        tb_updt.setShortcuts(['F2','Ctrl+U'])
+        tb_lstf.setShortcuts(['F3','Ctrl+O'])
+
+        tb_docs.triggered.connect(lambda:Support.get_docs(self))
+        tb_updt.triggered.connect(lambda:Support.get_updt(self))
+        tb_lstf.triggered.connect(lambda:Support.get_lstf(self))
+
+        toolbar.addAction(tb_docs)
+        toolbar.addSeparator()
+        toolbar.addSeparator()
+        toolbar.addAction(tb_updt)
+        toolbar.addSeparator()
+        toolbar.addSeparator()
+        toolbar.addAction(tb_lstf)
+
+    def page(self):
+        self.widget = QWidget()
+        self.layout = QVBoxLayout()
+        self.layout.setContentsMargins(30,15,30,15)
+        self.widget.setLayout(self.layout)
+        self.setCentralWidget(self.widget)
+
+        # Header.
+        h1 = QLabel('DeskPy')
+        h1.setMaximumHeight(100)
+        h1.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        h1.setStyleSheet('padding: 15px; margin-bottom: 10px; background: #204038; color: #FFF; border-radius: 5px; font-size: 16px; font-weight: 600;')
+        self.layout.addWidget(h1)
+        h2 = QLabel('Procesamiento de documentos')
+        h2.setStyleSheet('margin-bottom: 10px; color: #888; font-weight: 400;')
+        h2.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.layout.addWidget(h2)
+
+        # File dialog management.
+        self.sys_path = QLineEdit()
+        self.sys_path.setReadOnly(True)
+        self.sys_path.setPlaceholderText('Seleccionar directorio de trabajo')
+        self.sys_lnch = QPushButton()
+        self.sys_lnch.setIcon(QIcon('path.ico'))
+        self.sys_lnch.setCursor(Qt.CursorShape.PointingHandCursor)
+        self.sys_lnch.clicked.connect(self.filedialog)
+
+        wrap_1 = QHBoxLayout()
+        wrap_1.addWidget(self.sys_path)
+        wrap_1.addWidget(self.sys_lnch)
+
+        self.editf_id = QLineEdit()
+        self.editf_id.setPlaceholderText('Identificación')
+        self.editf_id.setMaximumWidth(170)
+        self.editf_id_bt = QPushButton()
+        self.editf_id_bt.setIcon(QIcon('backspace.ico'))
+        self.editf_id_bt.setCursor(Qt.CursorShape.PointingHandCursor)
+        self.editf_id_bt.clicked.connect(self.clear_field_id)
+
+        self.editf_fn = QLineEdit()
+        self.editf_fn.setPlaceholderText('Nombre y apellidos')
+        self.editf_fn.setContentsMargins(9,0,0,0)
+        self.editf_fn_bt = QPushButton()
+        self.editf_fn_bt.setIcon(QIcon('backspace.ico'))
+        self.editf_fn_bt.setCursor(Qt.CursorShape.PointingHandCursor)
+        self.editf_fn_bt.clicked.connect(self.clear_field_fn)
+
+        self.sys_lnch.setStyleSheet('background: none; border: none;')
+        self.editf_id_bt.setStyleSheet('background: none; border: none;')
+        self.editf_fn_bt.setStyleSheet('background: none; border: none;')
+
+        wrap_2 = QHBoxLayout()
+        wrap_2.addWidget(self.editf_id)
+        wrap_2.addWidget(self.editf_id_bt)
+        wrap_2.addWidget(self.editf_fn)
+        wrap_2.addWidget(self.editf_fn_bt)
+
+        self.crud_read = QPushButton('Leer')
+        self.crud_read.setIcon(QIcon('read.ico'))
+        self.crud_read.setStyleSheet('padding: 10px; border: 1px solid #CCC; border-radius: 2px;')
+        self.crud_read.setCursor(Qt.CursorShape.PointingHandCursor)
+        self.crud_read.clicked.connect(self.read_next)
+        self.crud_read.setDisabled(True)
+
+        self.crud_create = QPushButton('Procesar')
+        self.crud_create.setIcon(QIcon('create.ico'))
+        self.crud_create.setStyleSheet('padding: 10px; border: 1px solid #CCC; border-radius: 2px;')
+        self.crud_create.setCursor(Qt.CursorShape.PointingHandCursor)
+        self.crud_create.clicked.connect(self.fitem)
+        self.crud_create.setDisabled(True)
+
+        self.crud_auto = QPushButton('Auto')
+        self.crud_auto.setIcon(QIcon('auto.ico'))
+        self.crud_auto.setStyleSheet('padding: 10px; border: 1px solid #CCC; border-radius: 2px;')
+        self.crud_auto.setCursor(Qt.CursorShape.PointingHandCursor)
+        self.crud_auto.clicked.connect(self.auto_pilot)
+        self.crud_auto.setDisabled(True)
+
+        self.editf_id_bt.setShortcut('F6')
+        self.editf_fn_bt.setShortcut('F7')
+        self.sys_lnch.setShortcut('F5')
+        self.crud_read.setShortcut('Space')
+        self.crud_create.setShortcut('Return')
+        self.crud_auto.setShortcut('Ctrl+Return')
+
+        wrap_3 = QHBoxLayout()
+        wrap_3.addWidget(self.crud_read)
+        wrap_3.addWidget(self.crud_create)
+        wrap_3.addWidget(self.crud_auto)
+        wrap_3.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        wrap_3.setContentsMargins(0,20,0,0)
+
+        h3 = QLabel('Expedientes procesados:')
+        h3.setStyleSheet('margin: 10px 0; color: #888; font-weight: 400;')
+        self.textarea = QPlainTextEdit()
+        self.textarea.setPlaceholderText('Ready to use...')
+        self.textarea.setReadOnly(True)
+
+        self.layout.addLayout(wrap_1)
+        self.layout.addLayout(wrap_2)
+        self.layout.addLayout(wrap_3)
+        self.layout.addWidget(h3)
+        self.layout.addWidget(self.textarea)
+        self.layout.addStretch()
+
+    def filedialog(self):
+        self.workspace = QFileDialog.getExistingDirectory()
+        self.workspace += '/'
+        if self.workspace == '/': self.workspace = ''
+        self.sys_path.setText(self.workspace)
+        self.textarea.appendPlainText(f'Directorio configurado correctamente.')
+        self.crud_read.setDisabled(False)
+        self.crud_auto.setDisabled(False)
+        self.flist()
+
+    def flist(self):
+        self.tree = []
+        try:
+            list_workspace = os.listdir(self.workspace)
+            for leaf in list_workspace:
+                self.tree.append(f'{self.workspace}{leaf}')
+            self.counter = 0
+            self.textarea.appendPlainText('Directorio escaneado correctamente.')
+            self.textarea.appendPlainText(f'Sub-directorios a procesar: {len(self.tree)}')
+        except Exception as e: notification.notify(title = f'DeskPy',message = f'Hint: {e.__class__}\nLa ruta o carpeta a escanear no existe o no ha sido configurada, por favor configure una ruta válida.',timeout = 10)
+
+    def read_next(self):
+        try:
+            self.working_folder = self.tree[self.counter]
+            PDF.pdf_srch_text(self)
+            self.editf_id.setText(self.result_id)
+            self.editf_fn.setText(self.result_fn)
+            self.crud_read.setDisabled(True)
+            self.crud_create.setDisabled(False)
+            self.textarea.appendPlainText(f'\n{self.result_id} {self.result_fn} expediente listo para procesar...')
+        except IndexError as e: notification.notify(title = f'DeskPy',message = f'Hint: {e.__class__}\nNo hay más carpetas por procesar en el directorio {self.sys_path.text()}.',timeout = 5)
+
+    def fitem(self):
+            try:
+                subf = self.tree[self.counter]
+                subf = subf.split('/')
+                subf = f'../{subf[-3]}/{subf[-2]}/{subf[-1]}'
+                self.textarea.appendPlainText(f'\tProcesando... {self.counter+1}/{len(self.tree)} || {subf}')
+                self.counter += 1
+                self.crud_read.setDisabled(False)
+                self.crud_create.setDisabled(True)
+                try:
+                    edit_fields_upper = []
+                    edit_fields_upper.append(self.editf_id.text().upper())
+                    edit_fields_upper.append(self.editf_fn.text().upper())
+                    self.editf_id.setText(edit_fields_upper[0])
+                    self.editf_fn.setText(edit_fields_upper[1])
+                    PDF.app_deploy(self)
+                    self.editf_id.setText('')
+                    self.editf_fn.setText('')
+                except Exception as e: print(e)
+                if len(self.tree) == self.counter:
+                    self.textarea.appendPlainText('\nDirectorio de carpetas completado.')
+                    self.crud_read.setDisabled(True)
+                    self.crud_auto.setDisabled(True)
+                    notification.notify(title = f'DeskPy',message = 'No hay más carpetas por procesar.',timeout = 5)
+            except: self.textarea.appendPlainText('***No hay más carpetas por procesar***')
+
+    def auto_pilot(self):
+        while True:
+            self.crud_read.click()
+            self.crud_create.click()
+            if len(self.tree) == self.counter: break
+
+    def clear_field_id(self):
+        self.editf_id.setText('')
+        self.editf_id.setFocus()
+
+    def clear_field_fn(self):
+        self.editf_fn.setText('')
+        self.editf_fn.setFocus()
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    app.setStyleSheet("""
+            QToolBar{background: #fff;}
+            QLineEdit{padding: 4px; color: #444; background-color: #fff; border: 1px solid #797; border-radius: 5px;}
+            QPlainTextEdit{color: #00ff00; font-family: Monotype; background-color: #000; border: 1px solid #797; border-radius: 5px;}
+            QPushButton:hover{background: #fff;}
+        """)
+    win = Main()
+    sys.exit(app.exec())
